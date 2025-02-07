f26e87b39ae3cfd827363407c0355f88abdae7a4
Vinicius Atouguia <viniciusatouguia@outlook.com>
2025-02-07 01:04:13 -0300

    inclusão de projeto: cálculo de pedágio

---
+++ calcpedagio.py
@@ -0,0 +1,24 @@
+# Até 100 km: R$ 10,00
+# Entre 100 km e 200 km: R$ 20,00
+# Acima de 200 km: R$ 30,00
+
+km_primeiro = 100
+km_segundo = 200
+
+pedagio_1 = 10
+pedagio_2 = 20
+pedagio_3 = 30
+
+
+try:
+    distancia_percorrida = float(input('Digite a distância percorrida (km): '))
+    print(f'A distância percorrida é de {distancia_percorrida:.2f} km')
+    
+    if distancia_percorrida <= km_primeiro:
+        print(f'O valor do pedágio é de R$ {pedagio_1:.2f}')
+    elif distancia_percorrida > km_segundo:
+        print(f'O valor do pedágio é de R$ {pedagio_3:.2f}')
+    else:
+        print(f'O valor do pedágio é de R$ {pedagio_2:.2f}')
+
+except: print('Valor informado errado!')
